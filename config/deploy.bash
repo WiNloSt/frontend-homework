722b8bd21f2cacc4fef9077a6d603e49d7ed3432
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-07-13 09:09:54 +0700

    Add deployment script

---
+++ config/deploy.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#create neccessary directory
+mkdir -p build
+
+#build css
+yarn build-css
+
+#copy html files to build directory
+cp -r src/web/* build
+
+#remove scss file
+find build -name "*.scss" -delete
+
+#remove livejs script
+sed -i '' '/live/d' build/index.html
+
+#deploy to gh-pages
+yarn gh-pages -- -d build
